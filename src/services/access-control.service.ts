031076b10cef0f89aebe8ebcb2b24a9cd1d5b160
saleem-shaik-git <79854677+saleem-shaik-git@users.noreply.github.com>
2026-02-07 02:25:03 +0000

    Update access-control.service.ts

--- src/services/access-control.service.ts
+++ src/services/access-control.service.ts
@@ -1,1 +1,15 @@
+/**
+ * Access control service for educational content
+ * Technical Lead: Saleem Shaik
+ */
+
+type User = {
+  hasPaid: boolean;
+};
+
+export class AccessControlService {
+  canAccess(user: User): boolean {
+    return user.hasPaid;
+  }
+}
 
